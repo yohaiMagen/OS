e71124b6840f3ac3ca9564ed3c63be42adcd8365
Niv Keren <nivkeren@cs.huji.ac.il>
2017-06-14 12:05:18 +0300

    readme - DONE + comment fbr

--- Ex4/Fbr.h
+++ Ex4/Fbr.h
@@ -1,7 +1,3 @@
-//
-// Created by yohai on 5/26/17.
-//
-
 #ifndef EX4_FBR_H
 #define EX4_FBR_H
 
@@ -9,25 +5,38 @@
 #include <stdlib.h>
 #include "CacheAlg.h"
 
+/**
+ * the position of the block in the old, middle, new sections
+ */
 enum POS
 {
     OLD,
     NEW,
     MIDDLE,
-    NONE
+    NONE //** initialisation value */
 };
 
+/**
+ * frequancy Frequency-Based Replacement memory managent algorithm
+ */
 class Fbr: public CacheAlg
 {
 private:
-    unsigned long _usage_count;
-    unsigned long* _last_usage;
+    // an incremented variable to keep track of the number of times a block has been
+    // read, used as a clock
+    unsigned long long int _usage_count;
+    // array of the last time a block has been accessed
+    unsigned long long int* _last_usage;
+    // array of number of times a block has been accessed
     unsigned int* _ref_count;
+    // array of position in the buffer section of a block
     POS* _pos;
+    // the numbers of blocks
     int _blocks_num;
+    // size of each section
     unsigned int _old, _new, _middle;
+    // a map of all blocks in each section for each section
     std::map<unsigned long, char*> _oldMap, _newMap, _middleMap;
-
 
 public:
     Fbr(int blocks_num, double f_old, double f_new);
